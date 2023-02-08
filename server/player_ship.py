fc8d81e633791654d2fb928d2b7ab78c37c3d2a0
Michał Karaś <mjkaras93@gmail.com>
2023-02-08 19:55:44 +0100

    server - move endpoints and game_objects to separate files

--- server/player_ship.py
+++ server/player_ship.py
@@ -1,6 +1,33 @@
-from server.main import app
-from main import game
+from typing import Any
 
-@app.get("/")
-async def read_root():
-    player_ship = game.PLAYER_SHIP
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+from server.game_objects import game
+
+router = APIRouter()
+
+
+class ShipInstruction(BaseModel):
+    module: str
+    attribute: str
+    value: Any
+
+
+class ShipReading(BaseModel):
+    module: str
+    attribute: str
+
+
+@router.post("/set_attr")
+async def set_attr(ship_instruction: ShipInstruction):
+    module = getattr(game.player_ship.modules, ship_instruction.module)
+    setattr(module, ship_instruction.attribute, ship_instruction.value)
+    return {}
+
+
+@router.post("/get_attr")
+async def get_attr(ship_reading: ShipReading):
+    module = getattr(game.player_ship.modules, ship_reading.module)
+    value = getattr(module, ship_reading.attribute)
+    return {"value": value}
