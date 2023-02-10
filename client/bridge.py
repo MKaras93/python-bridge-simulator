acd2a7bdc5eea7c741e9bf5a27607b7642c3fb5f
Michał Karaś <mjkaras93@gmail.com>
2023-02-10 21:34:58 +0100

    add working client with cockpit module

---
+++ client/bridge.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+from client.http_client.api_client import APIClient, BaseClient
+
+CLIENT_CLASS = APIClient
+
+
+class BaseModule:
+    def __init__(self, ship: MotherShip, client):
+        self.ship: MotherShip = ship
+        self._client: BaseClient = client
+
+
+class Cockpit(BaseModule):
+    module_type = "cockpit"
+
+    @property
+    def target_angle(self) -> float:
+        return self._client.get_attribute(self.module_type, "target_angle")
+
+    @target_angle.setter
+    def target_angle(self, value: float):
+        self._client.set_attribute(self.module_type, "target_angle", value)
+
+    @property
+    def hyper_drive_percent(self) -> float:
+        return self._client.get_attribute(self.module_type, "hyper_drive_percent")
+
+    @hyper_drive_percent.setter
+    def hyper_drive_percent(self, value: float):
+        self._client.set_attribute(self.module_type, "hyper_drive_percent", value)
+
+    @property
+    def rotation_drive_percent(self) -> float:
+        return self._client.get_attribute(self.module_type, "rotation_drive_percent")
+
+    @rotation_drive_percent.setter
+    def rotation_drive_percent(self, value: float):
+        self._client.set_attribute(self.module_type, "rotation_drive_percent", value)
+
+    @property
+    def hyper_drive_timer(self) -> float:
+        return self._client.get_attribute(self.module_type, "hyper_drive_timer")
+
+    @hyper_drive_timer.setter
+    def hyper_drive_timer(self, value: float):
+        self._client.set_attribute(self.module_type, "hyper_drive_timer", value)
+
+
+class MotherShip:
+    def __init__(self):
+        self._client = CLIENT_CLASS()
+        self.cockpit: Cockpit = self._add_module(Cockpit)
+
+    def _add_module(self, module_class: type):
+        module = module_class(self, client=self._client)
+        return module
+
+
+MS = MotherShip()
---
+++ client/http_client/api_client.py
@@ -0,0 +1,47 @@
+import abc
+from typing import Union, Any
+from urllib import parse
+
+import requests
+
+
+class BaseClient(abc.ABC):
+    def set_attribute(self, module: str, attribute: str, value: Any) -> Any:
+        raise NotImplemented()
+
+    def get_attribute(self, module: str, attribute: str) -> Any:
+        raise NotImplemented()
+
+
+class APIClient:
+    API_URL = "http://localhost:8000"
+
+    def __init__(self):
+        self.session = requests.Session()
+
+    def _get_request(self, path="", **kwargs):
+        url = parse.urljoin(self.API_URL, path)
+        response = self.session.get(url, params=kwargs)
+        return response.json()
+
+    def _post_request(self, path="", payload: Union[dict, list] = None, **kwargs):
+        url = parse.urljoin(self.API_URL, path)
+        response = self.session.post(url, json=payload, params=kwargs)
+        return response.json()
+
+    def set_attribute(self, module: str, attribute: str, value: Any) -> Any:
+        payload = {
+            "module": module,
+            "attribute": attribute,
+            "value": value,
+        }
+        response_data = self._post_request("set_attr", payload)
+        return response_data["value"]
+
+    def get_attribute(self, module: str, attribute: str) -> Any:
+        payload = {
+            "module": module,
+            "attribute": attribute,
+        }
+        response_data = self._post_request("get_attr", payload)
+        return response_data["value"]
